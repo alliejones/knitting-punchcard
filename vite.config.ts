26b28d74f3e29c5c24ff8b0ba72c06ed49fb7eb0
Allie Jones <allie@alliejon.es>
2024-05-27 15:36:42 -0400

    Setup deployment to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.CI ? "knitting-punchcard" : "/",
   css: {
     modules: {
       localsConvention: "camelCaseOnly",
